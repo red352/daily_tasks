fbe01bf4ec334e1b0f30e3310ba1a1268d472d02
kinghongqiang <3502913960@qq.com>
2023-08-15 00:13:58 +0800

    fix

--- ikuuu_sign.py
+++ ikuuu_sign.py
@@ -5,14 +5,21 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 if __name__ == '__main__':
     request = requests.session()
-    params = {
-        "email": "shuffling36@gmail.com",
-        "passwd": "red123456"
-    }
-    re = request.post(url='https://ikuuu.art/auth/login', params=params, verify=False)
-    print(re.cookies)
-    re = request.post(url='https://ikuuu.art/user/checkin', verify=False)
-    print(re.json())
-    re = request.get(url='https://ikuuu.art/user/logout', verify=False)
-    print(re.cookies)
-    print("退出登入")
+    params = [
+        {
+            "email": "shuffling36@gmail.com",
+            "passwd": "red123456"
+        },
+        {
+            "email": "3502913960@qq.com",
+            "passwd": "4h7trn6.pyPs9YR"
+        }
+    ]
+    for param in params:
+        re = request.post(url='https://ikuuu.art/auth/login', params=param, verify=False)
+        print(re.cookies)
+        re = request.post(url='https://ikuuu.art/user/checkin', verify=False)
+        print(re.json())
+        re = request.get(url='https://ikuuu.art/user/logout', verify=False)
+        print(re.cookies)
+        print("退出登入")
