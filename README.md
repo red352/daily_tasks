4cb1966af01caa50d89ab62bc182c621ddb574a0
LuoYunXiao <1829462342@qq.com>
2024-08-03 17:59:20 +0800

    修改代码,提出环境变量

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # daily_tasks
+
+```python
+# 环境变量必填,可以置空但必填,数组以 `,`分割,以下示例值
+# EMAIL xxx@qq.com qq邮件地址
+# EMAIL_PWD xxxx qq邮件密码
+# TARGET_DOMAIN https://xxxx.com 签到ikuuu域名
+# LOGIN_EMAILS xxx@qq.com,sss@qq.com,sof@qq.com 签到的账号们
+# LOGIN_PASSWDS xxx,sss,ppp 签到的密码们
+# LOGIN_EMAILS_NOTIFY xxx,,xxx 账号通知的邮箱们
+```
--- ikuuu_sign.py
+++ ikuuu_sign.py
@@ -8,50 +8,55 @@ from email.mime.text import MIMEText
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+
+def is_blank(s):
+    return not s or s.strip() == ''
+
+
 if __name__ == '__main__':
-    msg_from = '3502913960@qq.com'
+    # 环境变量必填,可以置空但必填,数组以 `,`分割,以下示例值
+    # EMAIL xxx@qq.com qq邮件地址
+    # EMAIL_PWD xxxx qq邮件密码
+    # TARGET_DOMAIN https://xxxx.com 签到ikuuu域名
+    # LOGIN_EMAILS xxx@qq.com,sss@qq.com,sof@qq.com 签到的账号们
+    # LOGIN_PASSWDS xxx,sss,ppp 签到的密码们
+    # LOGIN_EMAILS_NOTIFY xxx,,xxx 账号通知的邮箱们
+
+    msg_from = environ.get("EMAIL")
     passwd = environ.get("EMAIL_PWD")
-
-    params = [
-        {
-            "email": 'shuffling36@gmail.com',
-            "passwd": environ.get("GMAIL_SHUFFLING36_PWD_IKUUU")
-        },
-        {
-            "email": '3502913960@qq.com',
-            "passwd": environ.get("QQ_3502913960_PWD_IKUUU")
-        },
-
-        {
-            "email": '1615703120@qq.com',
-            "passwd": environ.get("QQ_1615703120_PWD_IKUUU")
-        }
-    ]
+    target_domain = environ.get("TARGET_DOMAIN")
+    emails = environ.get('LOGIN_EMAILS').split(',')
+    passwds = environ.get('LOGIN_PASSWDS').split(',')
+    emails_to_notify = environ.get('LOGIN_EMAILS_NOTIFY').split(',')
+    params = [{'email': a, 'passwd': b} for a, b in zip(emails, passwds)]
+    notify_map = dict(zip(emails, emails_to_notify))
     print(params)
     request = requests.session()
 
     for param in params:
+        if is_blank(param['email']) or is_blank(param['passwd']):
+            continue
         msg = MIMEMultipart()
-        re = request.post(url='https://ikuuu.art/auth/login', params=param, verify=False)
+        re = request.post(url=target_domain + '/auth/login', params=param, verify=False)
         print(re.json()['msg'])
         msg.attach(MIMEText(re.json()['msg'] + '\r\n', 'plain', 'utf-8'))
-        re = request.post(url='https://ikuuu.art/user/checkin', verify=False)
-        print(re.json())
+        re = request.post(url=target_domain + '/user/checkin', verify=False)
+        try:
+            print(re.json())
+        except Exception as e:
+            msg.attach(MIMEText('签到出错' + '\r\n', 'plain', 'utf-8'))
+            continue
         if re.json()['ret'] == 1:
-            msg.attach(MIMEText(re.json()['msg'] + '\r\n', 'plain', 'utf-8'))
-            re = request.get(url='https://ikuuu.art/user/logout', verify=False)
+            re = request.get(url=target_domain + '/user/logout', verify=False)
             print('已退出账号')
             msg.attach(MIMEText('已退出账号', 'plain', 'utf-8'))
-            msg['Subject'] = 'ikuuu 每日流量签到提醒'
-            msg['From'] = msg_from
-            s = smtplib.SMTP_SSL('smtp.qq.com', 465)
-            s.login(msg_from, passwd)
-            if param['email'].__eq__('shuffling36@gmail.com'):
-                param['email'] = '1829462342@qq.com'
-            if param['email'].__eq__('1615703120@qq.com'):
-                param['email'] = '669858016@qq.com'
-            s.sendmail(msg_from, param['email'], msg.as_string())
-            print(f'邮件发送成功:{msg_from}  ---> {param["email"]}')
-            continue
-        re = request.get(url='https://ikuuu.art/user/logout', verify=False)
+        msg['Subject'] = 'ikuuu 每日流量签到提醒'
+        msg['From'] = msg_from
+        s = smtplib.SMTP_SSL('smtp.qq.com', 465)
+        s.login(msg_from, passwd)
+        email_to_notify = notify_map[param['email']]
+        if is_blank(email_to_notify):
+            email_to_notify = param['email']
+        s.sendmail(msg_from, email_to_notify, msg.as_string())
+        print(f'邮件发送成功:{msg_from}  ---> {param["email"]}')
         print('已退出账号')
